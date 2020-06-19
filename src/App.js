8a168afb8777aa448a13c3482ac6e18429b44052
parkerbacall <Pbacall2@gmail.com>
2020-06-19 16:02:36 -0600

    pokemon in state

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    pokemon: []
+  }
   
   componentDidMount(){ 
     fetch('https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json')
     .then(response => response.json())
-    .then(console.log)
+    .then(response => this.state.pokemon = response.pokemon)
   }
 
   render(){
